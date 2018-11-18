e930c900cb85aceba6a7442fdd2e899bf2ba5b47
Ana Arias-Gonzalez <ana.ariasgonz@gmail.com>
2018-11-18 20:21:55 +0000

    Done.

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,50 @@
 # Write your code here.
+require 'pry'
+
+def dictionary 
+  replacement = {
+    "hello" => "hi",
+    "to" => "2",
+    "two" => "2",
+    "too" => "2",
+    "for" => "4",
+    "four" => "4",
+    "be" => "b",
+    "you" => "u",
+    "at" => "@",
+    "and" => "&"
+  }
+end
+
+def word_substituter(tweet)
+     tweet.split(" ").collect do |word|
+      if dictionary.has_key?(word.downcase)
+        word = dictionary[word.downcase]
+      else
+        word
+    end
+  end.join(" ")
+end
+
+def bulk_tweet_shortener(array)
+  array.each do |tweet|
+    puts word_substituter(tweet)
+  end
+end
+
+def selective_tweet_shortener(tweet)
+    if(tweet.length > 140)
+      word_substituter(tweet)
+    else
+      tweet
+    end
+end
+
+def shortened_tweet_truncator(tweet)
+  short_tweet = word_substituter(tweet)
+  if short_tweet.length > 140
+    short_tweet[0...140]
+  else
+    short_tweet
+  end
+end
